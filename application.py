8aa3110e390b4d10cf80758a7f4c9e04448846ac
Kamil Malek <truckdriverbuck@gmail.com>
2025-04-26 12:53:45 +0200

    adds env extension for elasticbean

--- application.py
+++ application.py
@@ -1,11 +1,15 @@
+import os
 from flask import Flask
 
 application = Flask(__name__)
 
+ENV = os.environ.get('FLASK_ENV', 'developerskie')
+
 @application.route('/')
 def hello_world():
-    return("""
+    return(f"""
         <h1>Hello, Big Data z Pythonem!</h1>
+        <p>Środowisko <b>{ ENV }</b></p>
         <p>Przykład CI/CD - automatyczne wdrażanie!</p>
     """)
 
